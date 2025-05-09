ebceecb069d84ce82de6855086182410a688bce2
ErickCor24 <erick.cordovavit@ug.edu.ec>
2025-05-08 23:58:43 -0500

    Install angular-material & json-server, agroup all modules and creates all folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Agency
+
+    //Client
+
+    //Company
+
+    // Reserve
+
+    //Vehicle
+];
