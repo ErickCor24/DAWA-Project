c57e95cd61b1bbbc0032d8df273576c8a46b387f
ErickCor24 <erick.cordovavit@ug.edu.ec>
2025-05-11 00:29:30 -0500

    register company features

---
+++ src/app/components/company/company.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { RegisterCompanyComponent } from "./register-company/register-company.component";
+
+export const CompanyRoutes: Routes = [
+    {path: 'register', component: RegisterCompanyComponent}
+]
