2713513a7ab419041c7f12d5177f7aca5c8fcb9a
camillie15 <camillie15@users.noreply.github.com>
2025-05-11 12:19:44 -0500

    Create Vehicle Model

---
+++ src/app/models/Vehicle.ts
@@ -0,0 +1,16 @@
+export interface Vehicle {
+    idVehicle?: number;
+    idCompany: number;
+    idAgency: number;
+    brand: string;
+    model: string;
+    year: number;
+    plateNumber: string;
+    color: string;
+    type: string;
+    seats: number;
+    transmission: string;
+    fuelType: string;
+    isAvailable: boolean;
+    status: number;
+}
