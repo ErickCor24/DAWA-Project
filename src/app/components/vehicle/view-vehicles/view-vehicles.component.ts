d77024d8e535f8ced3ac81568047e29b58d1709e
camillie15 <camillie15@users.noreply.github.com>
2025-05-11 12:20:18 -0500

    Create view-vehicles Component

---
+++ src/app/components/vehicle/view-vehicles/view-vehicles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-vehicles',
+  imports: [],
+  templateUrl: './view-vehicles.component.html',
+  styleUrl: './view-vehicles.component.css'
+})
+export class ViewVehiclesComponent {
+
+}
