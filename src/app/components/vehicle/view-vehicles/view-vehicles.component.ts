98c0319ea2112e81928f7555108181e325f9375f
camillie15 <camillie15@users.noreply.github.com>
2025-05-12 00:07:17 -0500

    Update view-vehicle (cards)

--- src/app/components/vehicle/view-vehicles/view-vehicles.component.ts
+++ src/app/components/vehicle/view-vehicles/view-vehicles.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Vehicle } from '../../../models/Vehicle';
+import { VehicleService } from '../../../services/vehicle/vehicle.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-vehicles',
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './view-vehicles.component.html',
   styleUrl: './view-vehicles.component.css'
 })
 export class ViewVehiclesComponent {
+  vehicles: Vehicle[] = [];
 
+  constructor(private service: VehicleService, private router:  Router, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.getVehicles();
+  }
+
+  getVehicles(): void{
+    this.service.getVehicles().subscribe((data: Vehicle[]) => {
+      this.vehicles = data;
+    });
+  }
+
+  navigateToCreateVehicle(): void{
+    this.router.navigate(['/vehicle/create-vehicle']);
+  }
 }
