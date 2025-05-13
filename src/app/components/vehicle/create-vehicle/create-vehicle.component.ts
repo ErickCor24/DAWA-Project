430d61ec5262898da8b55b1065b6adbc615ab332
camillie15 <camillie15@users.noreply.github.com>
2025-05-13 15:33:08 -0500

    Update create-vehicle component

--- src/app/components/vehicle/create-vehicle/create-vehicle.component.ts
+++ src/app/components/vehicle/create-vehicle/create-vehicle.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { VehicleService } from '../../../services/vehicle/vehicle.service';
 
 @Component({
   selector: 'app-create-vehicle',
@@ -18,21 +19,30 @@ export class CreateVehicleComponent {
 
   formGroup!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private service: VehicleService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.formGroup = this.fb.group({
-      model: ['', Validators.required],
-      brand: ['', Validators.required],
-      type: ['', Validators.required],
-      year: ['', Validators.required],
-      plateNumber: ['', Validators.required],
+      model: ['', [Validators.required, Validators.minLength(3)]],
+      brand: ['', [Validators.required, Validators.minLength(3)]],
+      type: ['', [Validators.required, Validators.minLength(3)]],
+      year: ['', [Validators.required, Validators.min(1886), Validators.max(new Date().getFullYear() + 1)]],
+      plateNumber: ['ABC1234', [Validators.required, Validators.pattern(/^[A-Z]{3}[0-9]{4}$/)]],
       transmission: ['', Validators.required],
-      color: ['', Validators.required],
-      seats: ['', Validators.required],
-      fuelType: ['', Validators.required],
-      idAgency: ['', Validators.required]
+      color: ['', [Validators.required, Validators.minLength(3)]],
+      seats: ['', [Validators.required, Validators.min(2)]],
+      fuelType: ['', [Validators.required, Validators.minLength(3)]],
+      idAgency: ['', Validators.required],
+      poster: [''],
+      pricePerDay: ['', [Validators.required, Validators.min(1)]]
     });
+  }
+
+  submit(): void {
+    this.service.createVehicle(this.formGroup.value, 1).subscribe((createVehicle) => {
+      alert("vEHICLE CREATE");
+    })
+
   }
 }
 
