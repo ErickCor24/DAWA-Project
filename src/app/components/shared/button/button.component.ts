b2e1c483402477b6843384a4915fae1c86fe8058
Camillie Ayovi Villafuerte <138990540+camillie15@users.noreply.github.com>
2025-05-13 17:44:47 -0500

    Create Button Component (#10)
    Co-authored-by: camillie15 <camillie15@users.noreply.github.com>

---
+++ src/app/components/shared/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-button',
+  imports: [MatButtonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent implements OnInit{
+  
+  @Input() name: string = '';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  
+  @Output() clicked = new EventEmitter<string>;
+  
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.clicked.emit();
+  }
+}
