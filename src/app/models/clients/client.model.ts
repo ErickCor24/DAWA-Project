35c781727e9008ec8699cdd4b015bff8cd55442d
Dacb2134 <77132959+Dacb2134@users.noreply.github.com>
2025-05-15 06:06:15 -0500

    Creacion de carpeta clients en models
    Se añadieron los modelos para las entidades Client y UserClient.
    Estos modelos serán utilizados en los formularios, servicios y validaciones del modulo de cliente

---
+++ src/app/models/clients/client.model.ts
@@ -0,0 +1,13 @@
+
+export interface Client {
+  id: number;                
+  fullName: string;
+  email: string;
+  password: string;         
+  phone: string;
+  ci: string;
+  address?: string;
+  birthDate: string;
+  nationality: string;
+  status: boolean;
+}
