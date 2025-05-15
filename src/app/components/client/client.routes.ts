a12af2c1f67a81f7805b44f42252d9f0d8016f99
Dacb2134 <77132959+Dacb2134@users.noreply.github.com>
2025-05-15 06:30:11 -0500

    Creacion Componente login client
    Componente login con su ruta asociada

--- src/app/components/client/client.routes.ts
+++ src/app/components/client/client.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { ClientRegistrationComponent } from './client-registration/client-registration.component';
+import { ClientLoginComponent } from './client-login/client-login.component';
 
 export const clientRoutes: Routes = [
-  { path: 'register', component: ClientRegistrationComponent }
+  { path: 'register', component: ClientRegistrationComponent },
+  { path: 'login', component: ClientLoginComponent }
 ];
