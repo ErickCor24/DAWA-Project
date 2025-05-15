aa748200e15540d55c7dc7d53af3d4e6947b9b37
ErickCor24 <erick.cordovavit@ug.edu.ec>
2025-05-14 21:31:57 -0500

    Section companies asociates, filter and screen

--- src/app/components/company/company.routes.ts
+++ src/app/components/company/company.routes.ts
@@ -3,9 +3,11 @@ import { RegisterCompanyComponent } from "./register-company/register-company.co
 import { companyGuard } from "../auth/company.guard";
 import { LoginUserCompanyComponent } from "./login-user-company/login-user-company.component";
 import { ViewCompanyComponent } from "./view-company/view-company.component";
+import { ListCompaniesComponent } from "./list-companies/list-companies.component";
 
 export const CompanyRoutes: Routes = [
     {path: 'register', component: RegisterCompanyComponent},
     {path:'login', component: LoginUserCompanyComponent},
-    {path:'profile', component: ViewCompanyComponent, canActivate: [companyGuard]}
+    {path:'profile', component: ViewCompanyComponent, canActivate: [companyGuard]},
+    {path:'companies', component: ListCompaniesComponent, canActivate: [companyGuard]}
 ]
