fb781320e2bb1d7b52b8036a2bdfa243746c67be
Dacb2134 <77132959+Dacb2134@users.noreply.github.com>
2025-05-15 06:43:56 -0500

    Creacion client-search  y su ruta
    Se agregó el componente client-search para listar y buscar clientes
    También se configuró su ruta en client.routes.ts como '/search'.

--- src/app/components/client/client.routes.ts
+++ src/app/components/client/client.routes.ts
@@ -4,9 +4,11 @@ import { ClientRegistrationComponent } from './client-registration/client-regist
 import { ClientLoginComponent } from './client-login/client-login.component';
 import { ClientProfileViewComponent } from './client-profile-view/client-profile-view.component';
 import { ClientAuthGuard } from '../auth/clientguard/client-auth.guard';
+import { ClientSearchComponent } from './client-search/client-search.component';
 
 export const clientRoutes: Routes = [
   { path: 'register', component: ClientRegistrationComponent },
   { path: 'login', component: ClientLoginComponent },
-  { path: 'profile', component: ClientProfileViewComponent, canActivate: [ClientAuthGuard] }
+  { path: 'profile', component: ClientProfileViewComponent, canActivate: [ClientAuthGuard] },
+  { path: 'search', component: ClientSearchComponent }
 ];
