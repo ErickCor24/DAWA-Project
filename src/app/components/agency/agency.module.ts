0e144bcf0d5ef317b4bca50e870b66bc826c1f12
Carlos <carlos.dominguezgom@ug.edu.ec>
2025-05-17 17:07:44 -0500

    Add agency form, filter, styles and management component

--- src/app/components/agency/agency.module.ts
+++ src/app/components/agency/agency.module.ts
@@ -2,27 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AgencyRoutes } from './agency.routes';
+import { FormsModule } from '@angular/forms';
 
-// Material Modules
+// Angular Material
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-// Importa el componente como standalone
+// Componentes standalone
 import { AgencyManagementComponent } from './agency-management/agency-management.component';
+import { AgencyFormComponent } from './agency-form/agency-form.component';
+import { AgencyListComponent } from './agency-list/agency-list.component';
+import { AgencyFilterComponent } from './agency-filter/agency-filter.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(AgencyRoutes),
     MatTableModule,
     MatPaginatorModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+
+    // Importa componentes standalone 
+    AgencyManagementComponent,
+    AgencyFormComponent,
+    AgencyListComponent,
+    AgencyFilterComponent,
   ]
 })
-export class AgencyModule { }
-
+export class AgencyModule {}
