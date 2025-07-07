b5f9a15f9d912f7c6aadbf9024b1b4698bedd1f6
Dacb2134 <77132959+Dacb2134@users.noreply.github.com>
2025-07-07 08:38:47 -0500

    Update client.routes.ts
    Ajuste de guard usando roles

--- src/app/components/client/client.routes.ts
+++ src/app/components/client/client.routes.ts
@@ -5,13 +5,15 @@ import { ClientLoginComponent } from './client-login/client-login.component';
 import { ClientProfileViewComponent } from './client-profile-view/client-profile-view.component';
 import { ClientSearchComponent } from './client-search/client-search.component';
 import { ClientProfileEditComponent } from './client-profile-edit/client-profile-edit.component';
+import { authGuardGuard } from '../../Core/guards/auth-guard.guard';
 
 
 export const clientRoutes: Routes = [
   { path: 'register', component: ClientRegistrationComponent },
   { path: 'login', component: ClientLoginComponent },
-  { path: 'profile', component: ClientProfileViewComponent, /* canActivate: [ClientAuthGuard] */ },
-  { path: 'search', component: ClientSearchComponent, /* canActivate: [companyGuard] */ },
-  { path: 'edit-profile', component: ClientProfileEditComponent, /* canActivate: [ClientAuthGuard]  */},
+  { path: 'profile', component: ClientProfileViewComponent, canActivate: [authGuardGuard], data: { role: 'client' } },
+  { path: 'search', component: ClientSearchComponent, canActivate: [authGuardGuard], data: {role: 'company'}  },
+  { path: 'edit-profile', component: ClientProfileEditComponent, canActivate: [authGuardGuard], data: { role: 'client' } }, 
+ 
   {path:'', redirectTo:'/', pathMatch:'full'}
 ];
